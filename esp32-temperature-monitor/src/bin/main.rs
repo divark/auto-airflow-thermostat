6dd7338e3f9ea564b0cf15f950e262cd78187aca
Tyler Schmidt <tmschmid@protonmail.com>
2025-04-14 11:40:11 -0700

    Trim and downgrade esp dependencies.

--- esp32-temperature-monitor/src/bin/main.rs
+++ esp32-temperature-monitor/src/bin/main.rs
@@ -6,7 +6,7 @@ use esp32_temperature_monitor::temperature::station::{
 };
 use esp32_temperature_monitor::temperature::TemperatureUnit;
 use esp_hal::clock::CpuClock;
-use esp_hal::gpio::Flex;
+use esp_hal::gpio::{Level, OutputOpenDrain, Pull};
 use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use esp_hal::timer::timg::TimerGroup;
@@ -17,8 +17,6 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-extern crate alloc;
-
 #[main]
 fn main() -> ! {
     // generator version: 0.3.0
@@ -28,18 +26,8 @@ fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    esp_alloc::heap_allocator!(size: 72 * 1024);
-
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let _init = esp_wifi::init(
-        timg0.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
-
     let temperature_reading_pin = peripherals.GPIO0;
-    let pin_communicator = Flex::new(temperature_reading_pin);
+    let pin_communicator = OutputOpenDrain::new(temperature_reading_pin, Level::High, Pull::None);
     let mut temperature_monitor = Esp32TemperatureStation::new(pin_communicator);
 
     let temperature_units = TemperatureUnit::Fahrenheit;
@@ -47,9 +35,6 @@ fn main() -> ! {
     loop {
         let temperature = temperature_monitor.get_temperature(&temperature_units);
         info!("Temperature: {:.1}F", temperature);
-
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
--- esp32-temperature-monitor/src/temperature/station.rs
+++ esp32-temperature-monitor/src/temperature/station.rs
@@ -1,9 +1,8 @@
 use crate::temperature::TemperatureUnit;
 
-use embedded_hal::digital::PinState;
 use esp_hal::{
-    gpio::{Flex, Pull},
-    time::{Duration, Instant},
+    gpio::{Level, OutputOpenDrain, Pull},
+    time::{now, Duration, Instant},
 };
 use log::error;
 use temperature_monitor_interface::{Bit, BitReader, ByteReader, Endian};
@@ -15,41 +14,41 @@ pub trait TemperatureStation {
 /// Makes the device go to sleep (wait) for the specified
 /// duration.
 pub fn wait(duration: Duration) {
-    let delay_start = Instant::now();
-    while delay_start.elapsed() < duration {}
+    let delay_start = now();
+    while delay_start.duration_since_epoch() < duration {}
 }
 
 /// Returns the amount of time passed waiting for the Input pin to read back
 /// either High or Low.
-pub fn wait_for<'a>(input_pin: &Flex<'a>, desired_pin_state: PinState) -> Duration {
-    let mut time_passed = Instant::now();
-    let max_time_to_wait = Duration::from_secs(20);
+pub fn wait_for<'a>(input_pin: &OutputOpenDrain<'a>, desired_pin_state: Level) -> Duration {
+    let mut time_passed = now();
+    let max_time_to_wait = Duration::secs(20);
 
     loop {
         let has_reached_pin_state = match desired_pin_state {
-            PinState::High => input_pin.is_high(),
-            PinState::Low => input_pin.is_low(),
+            Level::High => input_pin.is_high(),
+            Level::Low => input_pin.is_low(),
         };
 
         if has_reached_pin_state {
             break;
         }
 
-        if time_passed.elapsed() >= max_time_to_wait {
+        if time_passed.duration_since_epoch() >= max_time_to_wait {
             error!("wait_for: Waited for longer than max time (2 seconds).");
             break;
         }
     }
 
-    time_passed.elapsed()
+    time_passed.duration_since_epoch()
 }
 
 pub struct PinBitReader<'a> {
-    pin: Flex<'a>,
+    pin: OutputOpenDrain<'a>,
 }
 
 impl<'a> PinBitReader<'a> {
-    pub fn new(pin: Flex<'a>) -> Self {
+    pub fn new(pin: OutputOpenDrain<'a>) -> Self {
         let mut new_bit_reader = Self { pin };
 
         new_bit_reader.prepare_to_read_bits();
@@ -61,24 +60,24 @@ impl<'a> PinBitReader<'a> {
     /// reading in the temperature.
     fn prepare_to_read_bits(&mut self) {
         self.pin.set_low();
-        wait(Duration::from_millis(18));
+        wait(Duration::millis(18));
 
         self.pin.set_high();
-        wait(Duration::from_micros(40));
+        wait(Duration::micros(40));
 
-        wait_for(&mut self.pin, PinState::High);
-        wait(Duration::from_micros(80));
+        wait_for(&mut self.pin, Level::High);
+        wait(Duration::micros(80));
     }
 }
 
 impl<'a> BitReader for PinBitReader<'a> {
     fn read_next_bit(&mut self) -> Option<Bit> {
         let (zero_bit_lower_bound_time, zero_bit_upper_bound_time) =
-            (Duration::from_micros(26), Duration::from_micros(28));
-        let one_bit_time = Duration::from_micros(70);
-        wait(Duration::from_micros(50));
+            (Duration::micros(26), Duration::micros(28));
+        let one_bit_time = Duration::micros(70);
+        wait(Duration::micros(50));
 
-        let time_passed = wait_for(&mut self.pin, PinState::Low);
+        let time_passed = wait_for(&mut self.pin, Level::Low);
         let is_zero_bit =
             time_passed >= zero_bit_lower_bound_time && time_passed <= zero_bit_upper_bound_time;
         if is_zero_bit {
@@ -95,10 +94,7 @@ pub struct Esp32TemperatureStation<'a> {
 }
 
 impl<'a> Esp32TemperatureStation<'a> {
-    pub fn new(mut input_pin: Flex<'a>) -> Self {
-        input_pin.enable_input(true);
-        input_pin.set_as_open_drain(Pull::None);
-
+    pub fn new(mut input_pin: OutputOpenDrain<'a>) -> Self {
         let pin_bit_reader = PinBitReader::new(input_pin);
         let byte_reader = ByteReader::new(pin_bit_reader);
         Self { byte_reader }
@@ -109,7 +105,7 @@ impl<'a> TemperatureStation for Esp32TemperatureStation<'a> {
     fn get_temperature(&mut self, temperature_unit_preference: &TemperatureUnit) -> f32 {
         // In the documentation for the DHT22, the collecting period must be
         // greater than 2 seconds, so we have to wait 2 seconds each time.
-        wait(Duration::from_secs(2));
+        wait(Duration::secs(2));
 
         let bit_reading_order = Endian::Big;
 
