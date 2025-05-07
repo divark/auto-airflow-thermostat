9f044061df4fd8692f7c8c51c2f99022f1e66fa2
Tyler Schmidt <tmschmid@protonmail.com>
2025-05-07 16:04:53 -0700

    Refactor esp32 temperature monitor to use temperature reading trait.

--- esp32-temperature-monitor/src/bin/main.rs
+++ esp32-temperature-monitor/src/bin/main.rs
@@ -1,42 +1,28 @@
 #![no_std]
 #![no_main]
 
-use embedded_dht_rs::dht11::Dht11;
-use esp_backtrace as _;
-use esp_hal::clock::CpuClock;
-use esp_hal::time::Duration;
-use esp_hal::{
-    delay::Delay,
-    gpio::{Level, OutputOpenDrain, Pull},
-    main,
-    peripherals::Peripherals,
-};
+use core::panic::PanicInfo;
+
+use esp32_temperature_monitor::Dht11TemperatureReader;
+use temperature_monitor_interface::{TemperatureReading, TemperatureUnits};
+
+use esp_hal::main;
 use esp_println::println;
+
+#[panic_handler]
+fn on_panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
 #[main]
 fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
-
-    let delay = Delay::new();
-
-    let od_for_dht11 = OutputOpenDrain::new(peripherals.GPIO8, Level::High, Pull::None);
-
-    let mut dht11 = Dht11::new(od_for_dht11, delay);
-
+    let mut temperature_reader = Dht11TemperatureReader::new();
+    let desired_temperature_units = TemperatureUnits::Fahrenheit;
     loop {
-        delay.delay(Duration::millis(5000));
+        let read_temperature = temperature_reader.read_temperature(desired_temperature_units);
 
-        match dht11.read() {
-            Ok(sensor_reading) => println!(
-                "DHT 11 Sensor - Temperature: {} °C, humidity: {} %",
-                sensor_reading.temperature, sensor_reading.humidity
-            ),
-            Err(error) => println!("An error occurred while trying to read sensor: {:?}", error),
-        }
-
-        println!("-----");
+        println!("Read temperature: {} F", read_temperature.get_reading());
     }
 }
--- esp32-temperature-monitor/src/lib.rs
+++ esp32-temperature-monitor/src/lib.rs
@@ -1,3 +1,48 @@
 #![no_std]
 
-pub mod temperature;
+use embedded_dht_rs::dht11::Dht11;
+
+use embedded_hal::digital::{InputPin, OutputPin};
+
+use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Level, OutputOpenDrain, Pull};
+
+use temperature_monitor_interface::{Temperature, TemperatureReading, TemperatureUnits};
+
+pub struct Dht11TemperatureReader<'a> {
+    dht11_reader: Dht11<OutputOpenDrain<'a>, Delay>,
+}
+
+impl Dht11TemperatureReader<'_> {
+    pub fn new() -> Self {
+        let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+        let peripherals = esp_hal::init(config);
+
+        let od_for_dht11 = OutputOpenDrain::new(peripherals.GPIO8, Level::High, Pull::None);
+        let delay = Delay::new();
+
+        let dht11_reader = Dht11::new(od_for_dht11, delay);
+
+        Self { dht11_reader }
+    }
+}
+
+impl TemperatureReading for Dht11TemperatureReader<'_> {
+    fn read_temperature(&mut self, desired_temperature_units: TemperatureUnits) -> Temperature {
+        let dht_reading = self.dht11_reader.read();
+
+        let read_temperature = match dht_reading {
+            Ok(sensor_reading) => {
+                let sensor_temperature_celsius = sensor_reading.temperature as f32;
+                Temperature::new(TemperatureUnits::Celsius, sensor_temperature_celsius)
+            }
+            Err(sensor_error) => panic!(
+                "An error has occurred while trying to read from the dht11 sensor: {:?}",
+                sensor_error
+            ),
+        };
+
+        read_temperature.convert_to(desired_temperature_units)
+    }
+}
